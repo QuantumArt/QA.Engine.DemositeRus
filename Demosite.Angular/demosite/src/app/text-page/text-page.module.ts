e9a02ab8b2701364a70f78c40e7fc292de682391
ZagrebinM <zagrebinm@quantumart.ru>
2022-12-14 18:01:10 +0300

    Add tabs behavior

--- Demosite.Angular/demosite/src/app/text-page/text-page.module.ts
+++ Demosite.Angular/demosite/src/app/text-page/text-page.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { QaEnginePageStructureModule } from '@quantumart/qa-engine-page-structure-angular';
 import { SafePipeModule } from '../pipes';
-import { DialogDirectiveModule, FoldboxDirectiveModule } from '../behaviors';
+import { DialogDirectiveModule, FoldboxDirectiveModule, TabsDirectiveModule } from '../behaviors';
 import { TextPageRoutingModule } from './text-page-routing.module';
 import { TextPageComponent } from './text-page.component';
 import { BreadcrumbsModule } from '../breadcrumbs/breadcrumbs.module';
@@ -15,7 +15,8 @@ import { BreadcrumbsModule } from '../breadcrumbs/breadcrumbs.module';
     SafePipeModule,
     FoldboxDirectiveModule,
     BreadcrumbsModule,
-    DialogDirectiveModule
+    DialogDirectiveModule,
+    TabsDirectiveModule
   ],
   declarations: [TextPageComponent],
 })
