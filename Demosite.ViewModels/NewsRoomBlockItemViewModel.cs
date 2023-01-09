fe810d2cb290fe4b909cdc03cdf4201b95625858
ARTQ\TimerbaevR <TimerbaevR@quantumart.ru>
2023-01-09 19:03:08 +0300

    rename PostDate

--- Demosite.ViewModels/NewsRoomBlockItemViewModel.cs
+++ Demosite.ViewModels/NewsRoomBlockItemViewModel.cs
@@ -6,7 +6,7 @@ namespace Demosite.ViewModels
     {
         public string Title { get; set; }
         public string Url { get; set; }
-        public DateTimeOffset? PostData { get; set; }
+        public DateTimeOffset? PostDate { get; set; }
         public string Brief { get; set; }
     }
 }
